9a8bcde4d117f96ce947dc9cc80d38429016d579
NicolasNucifora1997 <nuciforanicolas@gmail.com>
2021-03-07 11:52:20 +0100

    new input manager removed

---
+++ CyberHunter/Assets/Scripts/Menu/_MoveBuilding.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _MoveBuilding : MonoBehaviour
+{
+    [SerializeField] float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Move(-speed);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Debug.Log("Building destroyed");
+        Destroy(this.gameObject);
+    }
+
+    public void setSpeed(float speed){
+        this.speed = speed;
+    }
+}
